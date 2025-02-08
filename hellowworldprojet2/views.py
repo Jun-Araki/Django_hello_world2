11c926e0c7d97b5e2a31231ddc4cefdc308d5af1
Jun-Araki <jun7772006@gmail.com>
2025-02-08 18:23:48 +0900

    display hellow world

--- hellowworldprojet2/views.py
+++ hellowworldprojet2/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def your_view(request):
-    return HttpResponse("Hello, World!")
+def helloworldfunction(request):
+    returnedobject = HttpResponse('<h1>Hello World</h1>')
+    return returnedobject
+
+class HelloWorldClass(TemplateView):
+    template_name = 'hello.html'
